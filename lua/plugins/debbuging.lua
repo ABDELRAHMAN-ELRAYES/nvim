c29a1c3e2dab2af6883e2e3fc8248c92c6c34dc3
Abdelrahman Elrayes <abdelrahmanelrayes2@gmail.com>
2024-08-04 18:09:48 +0300

    Initial commit

---
+++ lua/plugins/debbuging.lua
@@ -0,0 +1,38 @@
+return {
+  {
+    -- for execution go lang 
+    "ray-x/go.nvim",
+    "fatih/vim-go",
+  },
+  {
+    "mfussenegger/nvim-dap",
+    dependencies = {
+      "nvim-neotest/nvim-nio",
+      "rcarriga/nvim-dap-ui",
+      "leoluz/nvim-dap-go",
+    },
+    config = function()
+      local dap = require("dap")
+      local dapui = require("dapui")
+
+      require("dapui").setup()
+      require("dap-go").setup()
+
+      dap.listeners.before.attach.dapui_config = function()
+        dapui.open()
+      end
+      dap.listeners.before.launch.dapui_config = function()
+        dapui.open()
+      end
+      dap.listeners.before.event_terminated.dapui_config = function()
+        dapui.close()
+      end
+      dap.listeners.before.event_exited.dapui_config = function()
+        dapui.close()
+      end
+
+      vim.keymap.set("n", "<Leader>dt", dap.toggle_breakpoint, {})
+      vim.keymap.set("n", "<Leader>dc", dap.continue, {})
+    end,
+  },
+}
---
+++ lua/vim-options.lua
@@ -0,0 +1,6 @@
+vim.api.nvim_set_option('expandtab', true)
+vim.api.nvim_set_option('tabstop', 2)
+vim.api.nvim_set_option('softtabstop', 2)
+vim.api.nvim_set_option('shiftwidth', 2)
+vim.g.mapleader = " "
+
